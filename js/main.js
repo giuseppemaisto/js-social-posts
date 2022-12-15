8a2edf1a45a8389c4935b08c12227a74e22b433d
giuseppemaisto <gmaisto98@gmail.com>
2022-12-15 16:04:55 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -55,3 +55,56 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let container = document.getElementById("container");
+
+//1 - prendiamo array di oggetti e lo stampiamo in html
+
+function StampaPost(){
+ container.innerHTML = ''; 
+ for(let key in posts){
+    let{id, content, media , author, created ,likes} = posts[key]
+
+    container.innerHTML += `
+    <div class= "post">
+        <div class="post_header">
+            <div class = "post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile" src="${author.image}">
+                    </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${author.name}</div>
+                    <div class="post-meta__time">${created}</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src="${media}" alt="${media}">
+
+        </div>
+
+        <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+            </div>
+        </div> 
+        </div>          
+
+
+    </div>
+     `
+
+ }
+ return container;
+}
+
+StampaPost();
